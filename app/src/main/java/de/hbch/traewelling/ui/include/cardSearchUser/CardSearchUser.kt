fd4faa1415ac1030dfad91a88c710c483c025576
coffeeandtrains <140796761+coffeeandtrains@users.noreply.github.com>
2023-07-28 15:06:05 +0200

    Created username search card

---
+++ app/src/main/java/de/hbch/traewelling/ui/include/cardSearchUser/CardSearchUser.kt
@@ -0,0 +1,90 @@
+package de.hbch.traewelling.ui.include.cardSearchUser
+
+import android.os.Handler
+import android.os.Looper
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import de.hbch.traewelling.R
+import de.hbch.traewelling.theme.AppTypography
+import de.hbch.traewelling.ui.composables.ButtonWithIconAndText
+
+@Composable
+fun CardSearchUser(
+    modifier: Modifier = Modifier,
+    searchAction: (String) -> Unit = { },
+) {
+    var text by rememberSaveable { mutableStateOf("") }
+
+    ElevatedCard(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.title_search_user),
+                style = AppTypography.headlineSmall
+            )
+            OutlinedTextField(
+                value = text,
+                singleLine = true,
+                onValueChange = {
+                    text = it
+                },
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.input_username)
+                    )
+                },
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Search
+                ),
+                keyboardActions = KeyboardActions(
+                    onSearch = {
+                        // TODO remove delay after compose migration
+                        val handler = Handler(Looper.getMainLooper())
+                        handler.postDelayed({
+                            searchAction(text)
+                        }, 500)
+                    }
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp)
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 4.dp),
+                horizontalArrangement = Arrangement.End
+            ) {
+                ButtonWithIconAndText(
+                    stringId = R.string.search,
+                    drawableId = R.drawable.ic_search,
+                    modifier = Modifier
+                        .padding(start = 8.dp),
+                    onClick = {
+                        searchAction(text)
+                    }
+                )
+            }
+        }
+    }
+}
